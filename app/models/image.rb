c119613505fb35b5faee36c6a2deb4b3f04dc8d9
Daniel Park <dpark189@gmail.com>
2018-07-13 09:37:01 -0400

    actually might need image url in image table

--- app/models/image.rb
+++ app/models/image.rb
@@ -11,11 +11,18 @@
 #
 
 class Image < ApplicationRecord
+  validate :ensure_photo
 
   belongs_to :post
   belongs_to :author,
     foreign_key: :author_id,
     class_name: :User
-
+    
   has_one_attached :photo
+
+  def ensure_photo
+    unless self.photo.attached?
+      errors[:photo] << "must be attached"
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_133136) do
+ActiveRecord::Schema.define(version: 2018_07_13_133619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2018_07_13_133136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "author_id"
+    t.string "image_url"
     t.index ["author_id"], name: "index_images_on_author_id"
     t.index ["post_id"], name: "index_images_on_post_id"
   end
