226ca9393d1f90786bc91c6fdc68166dafa2a5de
Daniel Park <dpark189@gmail.com>
2018-07-23 15:18:28 -0400

    fixed post explore item key error

--- frontend/components/explore/post_explore.jsx
+++ frontend/components/explore/post_explore.jsx
@@ -28,9 +28,9 @@ class PostExplore extends React.Component {
     }
     const items = Object.keys(this.props.posts).reverse().map( (key) => {
       return (
-          <div className="explore-posts">
+          <div key={key} className="explore-posts">
             <PostExploreItem
-              key={`asdf${key}`}
+              key={key}
               post={this.props.posts[key]}
               author={this.props.users[this.props.posts[key].author_id]}
               createLike={this.props.createLike}
